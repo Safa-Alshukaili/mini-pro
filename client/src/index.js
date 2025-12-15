8ef981bc849dad2ddfffe59db051a87357a9a955
Safa-Alshukaili <sssafa41@gmail.com>
2025-12-15 12:34:04 +0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 // client/src/index.js
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./app/Soter";   // ✅ تمّ التغيير هنا
+import { store } from "./Store/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
