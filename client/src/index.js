de0c26140d1be11cc833da701323f5ee77c76c34
Safa-Alshukaili <sssafa41@gmail.com>
2025-12-16 00:46:12 +0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./app/Store";
+import { store } from "./app/Soter";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
