df1a116f54bb96f1b1a37236a79bb0174f0b89c4
Safa-Alshukaili <sssafa41@gmail.com>
2025-12-15 22:55:17 +0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import { store } from "./Store/store";
+import { store } from "./app/Store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  /*<Provider store={store}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>*/
-  <App />
+  </Provider>
+  
 );
